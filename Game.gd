a0d96d2f4139008067e7706906caf137abf41436
Paul Guy <paulguy119@gmail.com>
2026-08-08 21:20:15 -0400

    Add back random new puzzle.
    Restores original functionality.

--- Game.gd
+++ Game.gd
@@ -1,10 +1,6 @@
 class_name Game
 extends Node2D
 
-const MIN_SIZE : Vector2i = Vector2i(3, 3)
-const MAX_SIZE : Vector2i = Vector2i(200, 200)
-var puzzle_size : Vector2i = Vector2i(40, 40)
-
 const FILE_EXTENSION : String = "arrows"
 
 @onready var background : Polygon2D = $Background
@@ -15,12 +11,16 @@ var title_logo : TextureRect = null
 var puzzledata : PuzzleData = null
 var puzzle : Puzzle = null
 
+var puzzle_size : Vector2i = Puzzle.DEFAULT_PUZZLE_SIZE
+var gen_params : RandGenParams = RandGenParams.new()
+
 func make_menu():
 	menu = load("res://Menu.tscn").instantiate()
 	add_child(menu)
 
 func make_puzzle(editor_mode : bool = false):
 	puzzle = load("res://Puzzle.tscn").instantiate()
+	puzzle.game = self
 	puzzle.play_mode = not editor_mode
 	add_child(puzzle)
 	puzzle.update_size(last_size)
@@ -68,6 +68,7 @@ var menu_main : Array[MenuItemDesc] = [
 func editor():
 	menu.destroy()
 	title_logo.queue_free()
+	puzzle_size = Puzzle.DEFAULT_PUZZLE_SIZE
 	make_puzzle(true)
 
 func quit_game():
@@ -84,7 +85,7 @@ func new_game_menu():
 
 var menu_new_game : Array[MenuItemDesc] = [
 	MenuSelectionDesc.new(load_puzzle, "Load"),
-	MenuSelectionDesc.new(random_puzzle, "Random"),
+	MenuSelectionDesc.new(random_menu, "Random"),
 	MenuSelectionDesc.new(main_menu, "Main Menu")
 ]
 
@@ -106,5 +107,82 @@ func select_return():
 		make_puzzle()
 		puzzle.set_data(puzzledata)
 
-func random_puzzle():
-	main_menu()
+func random_menu():
+	puzzle_size = Puzzle.DEFAULT_PUZZLE_SIZE
+	menu_random[0].value = puzzle_size.x
+	menu_random[1].value = puzzle_size.y
+	menu_random[2].value = gen_params.min_length
+	menu_random[3].value = gen_params.max_length
+	menu_advanced[0].value = gen_params.base_chance_num
+	menu_advanced[1].value = gen_params.base_chance_den
+	menu_advanced[2].value = gen_params.chance_mult_num
+	menu_advanced[3].value = gen_params.chance_mult_den
+	menu_advanced[4].value = gen_params.forward_pref_num
+	menu_advanced[5].value = gen_params.forward_pref_den
+	menu_advanced[6].value = gen_params.along_snake_pref_num
+	menu_advanced[7].value = gen_params.along_snake_pref_den
+	menu_advanced[8].value = gen_params.quadrant_pref_num
+	menu_advanced[9].value = gen_params.quadrant_pref_den
+	menu_advanced[10].value = gen_params.along_edge_pref_num
+	menu_advanced[11].value = gen_params.along_edge_pref_den
+	do_random_menu()
+
+func do_random_menu():
+	update_menu("Random", menu_random)
+
+var menu_random : Array[MenuItemDesc] = [
+	MenuValueDesc.new(puzzle_size.x, Puzzle.MIN_SIZE.x, Puzzle.MAX_SIZE.x, null, "Width"),
+	MenuValueDesc.new(puzzle_size.y, Puzzle.MIN_SIZE.y, Puzzle.MAX_SIZE.y, null, "Height"),
+	MenuValueDesc.new(Puzzle.MIN_LENGTH, Puzzle.MIN_LENGTH, Puzzle.MAX_LENGTH, min_length_change, "Min Length"),
+	MenuValueDesc.new(Puzzle.MIN_LENGTH, Puzzle.MIN_LENGTH, Puzzle.MAX_LENGTH, max_length_change, "Max Length"),
+	MenuSelectionDesc.new(advanced_menu, "Advanced >>"),
+	MenuDoubleSelectionDesc.new(new_game_menu, do_random, "Cancel", "Go"),
+]
+
+func min_length_change(val : int) -> int:
+	gen_params.min_length = min(val, gen_params.max_length)
+	return gen_params.min_length
+
+func max_length_change(val : int) -> int:
+	gen_params.max_length = max(val, gen_params.min_length)
+	return gen_params.max_length
+
+func advanced_menu():
+	menu.set_heading("Advanced")
+	menu.set_items(menu_advanced)
+
+var menu_advanced : Array[MenuItemDesc] = [
+	MenuValueDesc.new(0, 0, 1000, null, "Base Chance Num"),
+	MenuValueDesc.new(1, 1, 1000, null, "Base Chance Den"),
+	MenuValueDesc.new(0, 0, 1000, null, "Chance Mult Num"),
+	MenuValueDesc.new(1, 1, 1000, null, "Chance Mult Den"),
+	MenuValueDesc.new(0, 0, 1000, null, "Forward Pref Num"),
+	MenuValueDesc.new(1, 1, 1000, null, "Forward Pref Den"),
+	MenuValueDesc.new(0, 0, 1000, null, "Along Snake Pref Num"),
+	MenuValueDesc.new(1, 1, 1000, null, "Along Snake Pref Den"),
+	MenuValueDesc.new(0, 0, 1000, null, "Quadrant Pref Num"),
+	MenuValueDesc.new(1, 1, 1000, null, "Quadrant Pref Den"),
+	MenuValueDesc.new(0, 0, 1000, null, "Along Edge Pref Num"),
+	MenuValueDesc.new(1, 1, 1000, null, "Along Edge Pref Den"),
+	MenuSelectionDesc.new(do_random_menu, "Return")
+]
+
+func do_random():
+	# length params use functions so they're already updated
+	puzzle_size.x = menu_random[0].value
+	puzzle_size.y = menu_random[1].value
+	gen_params.base_chance_num = menu_advanced[0].value
+	gen_params.base_chance_den = menu_advanced[1].value
+	gen_params.chance_mult_num = menu_advanced[2].value
+	gen_params.chance_mult_den = menu_advanced[3].value
+	gen_params.forward_pref_num = menu_advanced[4].value
+	gen_params.forward_pref_den = menu_advanced[5].value
+	gen_params.along_snake_pref_num = menu_advanced[6].value
+	gen_params.along_snake_pref_den = menu_advanced[7].value
+	gen_params.quadrant_pref_num = menu_advanced[8].value
+	gen_params.quadrant_pref_den = menu_advanced[9].value
+	gen_params.along_edge_pref_num = menu_advanced[10].value
+	gen_params.along_edge_pref_den = menu_advanced[11].value
+	menu.destroy()
+	make_puzzle()
+	puzzle.generate_random(gen_params)
--- Puzzle.gd
+++ Puzzle.gd
@@ -10,6 +10,10 @@ const ZOOM_MIN : float = 1.0/8.0
 
 const BUTTON_SHADE_ALPHA : float = 0.5
 
+const MIN_SIZE : Vector2i = Vector2i(3, 3)
+const MAX_SIZE : Vector2i = Vector2i(200, 200)
+const DEFAULT_PUZZLE_SIZE : Vector2i = Vector2i(40, 40)
+
 const MIN_LENGTH : int = 3
 const MAX_LENGTH : int = 100
 
@@ -75,6 +79,7 @@ enum Action {
 @onready var d_pad : Control = $"Overlay/D-Pad"
 @onready var shade : Polygon2D = $"Shade"
 
+var game : Game
 var arrow_view : ArrowView = null
 var tile_size : Vector2i
 var map_size : Vector2i
@@ -124,7 +129,6 @@ var grow : bool = false:
 var action : Action = Action.SELECT
 var file_name : String = "Untitled"
 var select_return_func : Callable = return_to_game
-var gen_params : RandGenParams = RandGenParams.new()
 
 func _ready():
 	arrow_view = $"ArrowView"
@@ -273,8 +277,6 @@ func generate_random(gen_params : RandGenParams):
 func ui_event(e : InputEvent, c : Control):
 	if menu_display:
 		return
-
-	#get_viewport().set_input_as_handled()
 
 	var zoom_rel : float = 0.0
 	var zoom_pos : Vector2
@@ -505,56 +507,72 @@ func do_clear():
 	return_to_game()
 
 func random_menu():
+	menu_random[0].value = game.gen_params.min_length
+	menu_random[1].value = game.gen_params.max_length
+	menu_advanced[0].value = game.gen_params.base_chance_num
+	menu_advanced[1].value = game.gen_params.base_chance_den
+	menu_advanced[2].value = game.gen_params.chance_mult_num
+	menu_advanced[3].value = game.gen_params.chance_mult_den
+	menu_advanced[4].value = game.gen_params.forward_pref_num
+	menu_advanced[5].value = game.gen_params.forward_pref_den
+	menu_advanced[6].value = game.gen_params.along_snake_pref_num
+	menu_advanced[7].value = game.gen_params.along_snake_pref_den
+	menu_advanced[8].value = game.gen_params.quadrant_pref_num
+	menu_advanced[9].value = game.gen_params.quadrant_pref_den
+	menu_advanced[10].value = game.gen_params.along_edge_pref_num
+	menu_advanced[11].value = game.gen_params.along_edge_pref_den
+	do_random_menu()
+
+func do_random_menu():
 	update_menu("Randomize", menu_random)
 
 var menu_random : Array[MenuItemDesc] = [
-	MenuValueDesc.new(gen_params.min_length, MIN_LENGTH, MAX_LENGTH, min_length_change, "Min Length"),
-	MenuValueDesc.new(gen_params.max_length, MIN_LENGTH, MAX_LENGTH, max_length_change, "Max Length"),
+	MenuValueDesc.new(MIN_LENGTH, MIN_LENGTH, MAX_LENGTH, min_length_change, "Min Length"),
+	MenuValueDesc.new(MIN_LENGTH, MIN_LENGTH, MAX_LENGTH, max_length_change, "Max Length"),
 	MenuSelectionDesc.new(advanced_menu, "Advanced >>"),
 	MenuDoubleSelectionDesc.new(return_to_game, do_random, "Cancel", "Go"),
 ]
 
 func min_length_change(val : int) -> int:
-	gen_params.min_length = min(val, gen_params.max_length)
-	return gen_params.min_length
+	game.gen_params.min_length = min(val, game.gen_params.max_length)
+	return game.gen_params.min_length
 
 func max_length_change(val : int) -> int:
-	gen_params.max_length = max(val, gen_params.min_length)
-	return gen_params.max_length
+	game.gen_params.max_length = max(val, game.gen_params.min_length)
+	return game.gen_params.max_length
 
 func advanced_menu():
-	menu.set_heading("Advanced")
-	menu.set_items(menu_advanced)
+	update_menu("Advanced", menu_advanced)
 
 var menu_advanced : Array[MenuItemDesc] = [
-	MenuValueDesc.new(gen_params.base_chance_num, 0, 1000, null, "Base Chance Num"),
-	MenuValueDesc.new(gen_params.base_chance_den, 1, 1000, null, "Base Chance Den"),
-	MenuValueDesc.new(gen_params.chance_mult_num, 0, 1000, null, "Chance Mult Num"),
-	MenuValueDesc.new(gen_params.chance_mult_den, 1, 1000, null, "Chance Mult Den"),
-	MenuValueDesc.new(gen_params.forward_pref_num, 0, 1000, null, "Forward Pref Num"),
-	MenuValueDesc.new(gen_params.forward_pref_den, 1, 1000, null, "Forward Pref Den"),
-	MenuValueDesc.new(gen_params.along_snake_pref_num, 0, 1000, null, "Along Snake Pref Num"),
-	MenuValueDesc.new(gen_params.along_snake_pref_den, 1, 1000, null, "Along Snake Pref Den"),
-	MenuValueDesc.new(gen_params.quadrant_pref_num, 0, 1000, null, "Quadrant Pref Num"),
-	MenuValueDesc.new(gen_params.quadrant_pref_den, 1, 1000, null, "Quadrant Pref Den"),
-	MenuValueDesc.new(gen_params.along_edge_pref_num, 0, 1000, null, "Along Edge Pref Num"),
-	MenuValueDesc.new(gen_params.along_edge_pref_den, 1, 1000, null, "Along Edge Pref Den"),
-	MenuSelectionDesc.new(random_menu, "Return")
+	MenuValueDesc.new(0, 0, 1000, null, "Base Chance Num"),
+	MenuValueDesc.new(1, 1, 1000, null, "Base Chance Den"),
+	MenuValueDesc.new(0, 0, 1000, null, "Chance Mult Num"),
+	MenuValueDesc.new(1, 1, 1000, null, "Chance Mult Den"),
+	MenuValueDesc.new(0, 0, 1000, null, "Forward Pref Num"),
+	MenuValueDesc.new(1, 1, 1000, null, "Forward Pref Den"),
+	MenuValueDesc.new(0, 0, 1000, null, "Along Snake Pref Num"),
+	MenuValueDesc.new(1, 1, 1000, null, "Along Snake Pref Den"),
+	MenuValueDesc.new(0, 0, 1000, null, "Quadrant Pref Num"),
+	MenuValueDesc.new(1, 1, 1000, null, "Quadrant Pref Den"),
+	MenuValueDesc.new(0, 0, 1000, null, "Along Edge Pref Num"),
+	MenuValueDesc.new(1, 1, 1000, null, "Along Edge Pref Den"),
+	MenuSelectionDesc.new(do_random_menu, "Return")
 ]
 
 func do_random():
 	# length params use functions so they're already updated
-	gen_params.base_chance_num = menu_advanced[0].value
-	gen_params.base_chance_den = menu_advanced[1].value
-	gen_params.chance_mult_num = menu_advanced[2].value
-	gen_params.chance_mult_den = menu_advanced[3].value
-	gen_params.forward_pref_num = menu_advanced[4].value
-	gen_params.forward_pref_den = menu_advanced[5].value
-	gen_params.along_snake_pref_num = menu_advanced[6].value
-	gen_params.along_snake_pref_den = menu_advanced[7].value
-	gen_params.quadrant_pref_num = menu_advanced[8].value
-	gen_params.quadrant_pref_den = menu_advanced[9].value
-	gen_params.along_edge_pref_num = menu_advanced[10].value
-	gen_params.along_edge_pref_den = menu_advanced[11].value
-	generate_random(gen_params)
+	game.gen_params.base_chance_num = menu_advanced[0].value
+	game.gen_params.base_chance_den = menu_advanced[1].value
+	game.gen_params.chance_mult_num = menu_advanced[2].value
+	game.gen_params.chance_mult_den = menu_advanced[3].value
+	game.gen_params.forward_pref_num = menu_advanced[4].value
+	game.gen_params.forward_pref_den = menu_advanced[5].value
+	game.gen_params.along_snake_pref_num = menu_advanced[6].value
+	game.gen_params.along_snake_pref_den = menu_advanced[7].value
+	game.gen_params.quadrant_pref_num = menu_advanced[8].value
+	game.gen_params.quadrant_pref_den = menu_advanced[9].value
+	game.gen_params.along_edge_pref_num = menu_advanced[10].value
+	game.gen_params.along_edge_pref_den = menu_advanced[11].value
+	generate_random(game.gen_params)
 	return_to_game()
